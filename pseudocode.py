8b5c738b3a6e3f8ff9d179fbcd13703d2696dd44
Jlim331 <54864392+Jlim331@users.noreply.github.com>
2019-10-02 14:18:28 -0600

    Update pseudocode.py

---
+++ pseudocode.py
@@ -0,0 +1,66 @@
+# Title screen will show first
+# Option of play and how to play
+
+# When clicked how to play a instruction will be printed on screen
+
+"""When clicked play, the game will commence and the player will find he/she
+in a maze and will have to go through the maze in order to get out, player
+will then have the choice to go through path a, b or c"""
+
+# Path a
+"""Randomly generate a level 0 mob"""
+
+"""Player will now have two choices to go through path aa, ab or ac"""
+# Path b
+
+# Path c
+
+# Path aa
+"""Randomly generate a level 1 mob"""
+
+"""Player will now have the choice to go to path aaa, aab, or aac"""
+# Path ab
+
+# Path ac
+
+# Path ba
+
+# Path bb
+
+# Path bc
+
+# Path ca
+
+# Path cb
+
+# Path cc
+
+# Path aaa
+"""Randomly generates a level 2 mob"""
+
+"""Player will have a choice to go to path aaaa, aaab or aaac"""
+
+# Path aab
+
+# Path aac
+
+# Path aba
+
+# Path abb
+
+# Path abc
+
+# Path aca
+
+# Path acb
+
+# Path acc
+
+# Path aaaa
+"""Randomly generates a level 3 mob"""
+
+"""Player will now have the choice to go to path aaaaa, aaaab, or aaaac"""
+# Path aaaaa
+"""Player will fight mob boss"""
+
+"""Game is completed once boss is defeated"""
