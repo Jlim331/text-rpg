ac2f9e7d4d85035b650b42c3cb10619bcb9ee1b7
Jlim331 <54864392+Jlim331@users.noreply.github.com>
2019-10-22 14:15:19 -0600

    Create Dictionary and list.py

---
+++ Dictionary and list.py
@@ -0,0 +1,32 @@
+Weapons = {
+    "Stick": {
+        "ATTACK": 3,
+        "DURABILITY": 5,
+        },
+    "Iron Sword": {
+        "ATTACK": 15,
+        "DURABILITY": 20,
+        "SHARPNESS": 10,
+        }
+    }
+
+Mobs = {
+    "Spider": {
+        "ATTACK": 3,
+        "HP": 10,
+        "VITALITY": 10,
+        "DEFENSE": 10,
+        "LUCK": 0,
+        }
+    }
+
+Charater = {
+    "inventory":["map", "flashlight"],
+    "stats": {
+        "ATTACK": 10,
+        "HP": 100,
+        "VITALITY": 10,
+        "DEFENSE": 10,
+        "LUCK": 0,
+        }
+}
