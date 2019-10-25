feb05cbfd5cf70ca9126a5b3a889763ff68510b1
Jlim331 <54864392+Jlim331@users.noreply.github.com>
2019-10-24 20:08:30 -0600

    Updated code to the PEP8 style guide Updated code to agree with assignment minimum

--- properties.py
+++ properties.py
@@ -1,19 +1,24 @@
+# Josh Lim
+# Comp Sci 30 P4
+# 10/24/2019
+# Nested dictionaries for game properties
+
 def nestedDictPrinter(dictName):
-    """Prints out content in a nested dictionary up to 2 layers,
+    """Prints out content in a nested dictionary with up to 2 layers,
     takes dictName as the dictionary name.
     """
     for dictName, key in dictName.items():
         """loops through the keys in the nested dictionary, dictName, and then
         prints the nested dictionary names along with its values.
         """
-        print(dictName.title()) # prints nested dictionary name
+        print(dictName.title() + ":") # prints nested dictionary name
         for items in key:
             """loops through nested dictionary key values and and prints the
             key along with the value with a tab before.
             """
-            print(f"\t{items}: {info[items]}")
-# Nested dictinoary containing information for weapons
-weapons = {
+            print(f"\t{items.title()} - {key[items]}")
+# Nested dictionary containing information for inventory
+inventory = {
     "stick": {
         "ATTACK": 3,
         "DURABILITY": 5,
@@ -24,47 +29,47 @@ weapons = {
         "SHARPNESS": 10,
         }
     }
-# Nested dictinoary containing information for mobs
+# Nested dictionary containing information for mobs
 mobs = {
     "spider": {
         "ATTACK": 3,
-        "HP": 10,
+        "HIT POINTS": 10,
         "VITALITY": 10,
         "DEFENSE": 10,
         "LUCK": 0,
         }
     }
-# Nested dictinoary containing information for hero
-hero = {
-    "inventory": ["map", "flashlight"],
+# Nested dictionary containing information for character
+character = {
     "stats": {
         "ATTACK": 10,
-        "HP": 100,
+        "HIT POINTS": 100,
         "VITALITY": 10,
         "DEFENSE": 10,
         "LUCK": 0,
         }
 }
-for character, info in hero.items():
-    """loops through the nested dictionary, character and its keys and then
-    prints out the nested dictionary name.
-    """
-    print(character.title())
-    if character == "inventory":
-        for items in key:
-            """loops through nested dictionary key values and and prints the
-            key along with the value with a tab before if the key is
-            "inventory"
-            """
-            print(f"\t{items}")
-    if character == "stats":
-        for items in key:
-            """loops through nested dictionary key values and and prints the
-            key along with the value with a tab before if the key is
-            "stats"
-            """
-            print(f"\t{items}: {info[items]}")
+# Nested dictionary containing information for locations
+locations = {
+    "path 1": {
+        "description": "You have encountered a level 1 mob!",
+        },
+    "path 2": {
+        "description":"You are greeted with 2 paths, which will you choose?",
+        },
+    "path 3": {
+        "description": "You find yourself in a forest, what will you do?",
+    }
+}
+print("\nInventory")
 # Prints out the content of the weapons nested dictionary
-nestedDictPrinter(weapons)
+nestedDictPrinter(inventory)
+print("\nMobs")
 # Prints out the content of the mobs nested dictionary
 nestedDictPrinter(mobs)
+print("\nCharacter")
+# Prints out the content of the character nested dictionary
+nestedDictPrinter(character)
+print("\nLocations")
+# Prints out the content of the locations nested dictionary
+nestedDictPrinter(locations)
